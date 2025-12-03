34be6bc2098930480aef73c3919b3a4e9d375f4a
coder33 <coder33@yandex.ru>
2025-12-03 15:17:49 +0400

    ошибка сделана читаемее

--- csvtoapi/consumer.go
+++ csvtoapi/consumer.go
@@ -48,7 +48,7 @@ func consume(wg *sync.WaitGroup, config models.Config) chan models.Raw {
 			raw.Elems = strings.Split(line, config.Separator)
 
 			if len(raw.Headers) != len(raw.Elems) {
-				log.Printf("\n[Error] in line! %s \n!!! [>>>] Check the separator!!! header have %d elements; row have %d elements;\n", line, len(raw.Headers), len(raw.Elems))
+				log.Printf("\n[Error] LINE WAS IGNORED!!! %s \n!!! [>>>] Check the separator!!! header have %d elements; row have %d elements;\n", line, len(raw.Headers), len(raw.Elems))
 				continue
 			}
 
